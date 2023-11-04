015b5747e2878900a0cfb8c194be0a69f75a39dc
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-05 02:02:05 +0600

    google log in added

--- src/Pages/Login/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin.jsx
@@ -1,9 +1,33 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 const SocialLogin = () => {
+    const {googleLogin} = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogin = (login) => {
+        login()
+        .then(res => {
+            console.log(res)
+            toast.success('Login Successful')
+            navigate('/')
+        })
+        .catch(error => {
+            console.log(error);
+            toast.error(error.message);
+        })
+
+    }
+
+
   return (
     <div>
       <div className="flex items-center justify-center">
         {/* <h1 className="mr-3">Or Continue With </h1> */}
         <button
+        onClick={() => handleLogin(googleLogin)}
           className="btn mt-3 capitalize text-sm font-semibold mr-3"
         >
           Or Continue With Google
