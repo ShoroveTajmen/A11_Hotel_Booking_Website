244c3180b6e567bed8e89525e638476cd2e7e778
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-07 05:08:31 +0600

    added the delete functionality to delete room booking before 1 day from the booking date

--- src/Pages/MyBookings/MyBookingList.jsx
+++ src/Pages/MyBookings/MyBookingList.jsx
@@ -1,7 +1,61 @@
+/* eslint-disable react/prop-types */
+
+import moment from "moment";
+import Swal from "sweetalert2";
+
+/* eslint-disable no-unused-vars */
 const MyBookingList = ({ room, findUserRoom, setFindUserRoom }) => {
   const { _id, roomDescription, roomPrice, selectedDate, userEmail, roomPic } =
     room || {};
-  console.log(userEmail);
+  //   console.log(userEmail);
+
+  //tract current date
+  const currentDate = moment();
+  const formattedDate = moment(selectedDate, "DD/MM/YYYY");
+//   console.log(currentDate);
+  // Calculate the date before which a booking can be deleted (e.g., one day before the booking date)
+  const deleteBeforeBookingDate = formattedDate.clone().subtract(1, "days");
+//   console.log(deleteBeforeBookingDate);
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    // Check if the current date is before the deleteBeforeDate
+    if (currentDate.isBefore(deleteBeforeBookingDate)) {
+      console.log("booking can be deleted.");
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      })
+      .then(result => {
+        if(result.isConfirmed){
+            fetch(`http://localhost:5001/roomBooks/${_id}`, {
+                method: "DELETE"
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.deletedCount > 0){
+                    Swal.fire("Deleted!", "Your product has been deleted.", "success");
+                    const remaining = findUserRoom.filter(room => room._id !== _id)
+                    setFindUserRoom(remaining)
+                }
+            })
+        }
+      })
+    } else {
+      console.log("Booking cant be deleted.");
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Booking can not be deleted!",
+      });
+    }
+  };
 
   return (
     <div>
@@ -22,13 +76,12 @@ const MyBookingList = ({ room, findUserRoom, setFindUserRoom }) => {
             </div>
             <div className="flex text-sm gap-2">
               <button
+                onClick={() => handleDelete(_id)}
                 className="bg-black text-white p-3  font-bold  mt-[50px]"
               >
                 Delete Booking
               </button>
-              <button
-                className="bg-black text-white p-3  font-bold  mt-[50px]"
-              >
+              <button className="bg-black text-white p-3  font-bold  mt-[50px]">
                 Update Booking
               </button>
             </div>
