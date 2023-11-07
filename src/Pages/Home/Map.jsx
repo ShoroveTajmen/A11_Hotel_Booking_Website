53240de1e62133f146dfff56d573a71f36864fe9
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-08 02:43:47 +0600

    lottie animation added in the home page

--- src/Pages/Home/Map.jsx
+++ src/Pages/Home/Map.jsx
@@ -1,13 +1,22 @@
+import Lottie from "lottie-react";
+import animation from '../../Animation - 1699389033345.json';
+
 const Map = () => {
   return (
     <div className="mt-8">
-      <div className="w-[700px] h-[550px] mx-auto">
-        <iframe
-          src="https://maps.google.com/maps?q=dhaka&amp;t=&amp;z=9&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
-          frameBorder="0"
-          scrolling="no"
-          style={{ width: "700px", height: "500px" }}
-        ></iframe>
+      <div className=" mx-auto border border-red-700 flex justify-center gap-8">
+        <div className='w-[500px] h-[500px]'>
+        <Lottie animationData={animation} loop={true} />
+        </div>
+        <div className="">
+          {" "}
+          <iframe
+            src="https://maps.google.com/maps?q=dhaka&amp;t=&amp;z=9&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+            frameBorder="0"
+            scrolling="no"
+            style={{ width: "500px", height: "500px", borderRadius:"30px" }}
+          ></iframe>
+        </div>
       </div>
     </div>
   );
