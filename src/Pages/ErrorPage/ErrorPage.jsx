de38a6f6b5ba00e0a2b546cd672796ef3dd614ac
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-08 02:50:32 +0600

    lottie animation added to the errorpage

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,10 @@
-
+import Lottie from "lottie-react";
+import animation2 from '../../Animation - 1699389918336.json';
 
 const ErrorPage = () => {
     return (
-        <div className="h-screen flex justify-center items-center text-3xl text-red-600">
-        Page Not Found
+        <div className="h-screen flex justify-center items-center ">
+        <Lottie animationData={animation2} loop={true} />
       </div>
     );
 };
